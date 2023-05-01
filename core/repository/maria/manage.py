b5f5b2f967528c6df95c8ec009f3be9115e25661
wookjaejo <jwj0104@gmail.com>
2023-05-01 12:57:00 +0900

    일별 차트 및 월 차트 관리 코드 개선

--- core/repository/maria/manage.py
+++ core/repository/maria/manage.py
@@ -4,8 +4,9 @@ import pandas as pd
 from pandas import DataFrame
 from pykrx.website.krx.market.core import 상장종목검색
 
+from base.timeutil import YearMonth
 from core.repository.krx import get_market_ohlcv_by_date
-from core.repository.maria.conn import maria_home, MariaConnection
+from core.repository.maria.conn import MariaConnection, maria_home
 
 db = maria_home()
 
@@ -31,7 +32,6 @@ def create_chart_table(table_name: str):
             """
         )
         conn.commit()
-
         conn.query(f"create index chart_code_index on {table_name} (code);")
         conn.query(f"create index chart_date_index on {table_name} (date);")
         conn.commit()
@@ -92,6 +92,85 @@ def update_stocks() -> DataFrame:
     return df
 
 
-def upload_from_krx():
+def upload_chart_from_krx():
     stocks = update_stocks()
     update_chart(list(stocks["code"]))
+
+
+def create_month_chart_table(table_name: str):
+    with MariaConnection() as conn:
+        conn.query(
+            f"""
+            create table {table_name}
+            (
+                code        varchar(8) null,
+                date        date null,
+                open        int null,
+                high        int null,
+                low         int null,
+                close       int null,
+                vol         bigint null,
+                vol_last    bigint null,
+                val         bigint null,
+                val_last    bigint null,
+                cap         bigint null,
+                shares      bigint null,
+                primary key (code, date)
+            );
+            """
+        )
+        conn.commit()
+        conn.query("create index if not exists chart_code_index on month_chart (code);")
+        conn.query("create index if not exists chart_date_index on month_chart (date);")
+        conn.commit()
+
+
+def insert_month_chart(year: int, month: int):
+    with MariaConnection() as conn:
+        conn.query(f"""insert into month_chart (
+        SELECT
+            code,
+            MAX(date)                                                                    as date,
+            cast(SUBSTRING_INDEX(MIN(CONCAT(date, '_', open)), '_', -1) as unsigned)     as open,
+            cast(MAX(high) as unsigned)                                                  as high,
+            cast(MIN(low) as unsigned)                                                   as low,
+            cast(SUBSTRING_INDEX(MAX(CONCAT(date, '_', close)), '_', -1) as unsigned)    as close,
+            cast(SUM(vol) as unsigned)                                                   as vol,
+            cast(SUBSTRING_INDEX(MAX(CONCAT(date, '_', vol)), '_', -1) as unsigned)      as vol_last,
+            cast(SUM(val) as unsigned)                                                   as val,
+            cast(SUBSTRING_INDEX(MAX(CONCAT(date, '_', val)), '_', -1) as unsigned)      as val_last,
+            cast(SUBSTRING_INDEX(MAX(CONCAT(date, '_', cap)), '_', -1) as unsigned)      as cap,
+            cast(SUBSTRING_INDEX(MAX(CONCAT(date, '_', shares)), '_', -1) as unsigned)   as shares
+        FROM chart
+        where year(date) = {year} and month(date) = {month}
+        group by code, year(date), month(date)
+        order by date);
+        """)
+        conn.commit()
+        conn.close()
+
+
+def upload_month_chart():
+    todate = date.today().strftime('%Y%m%d')
+    table_name = f"month_chart_{todate}"
+
+    drop_table_if_exists(table_name)
+    create_month_chart_table(table_name)
+
+    dates = pd.read_sql("select distinct date from chart", maria_home())["date"]
+    yms = sorted({YearMonth.from_date(d) for d in dates})
+    assert len(yms) == len(set(yms))
+    for ym in min(yms).to(max(yms)):
+        print(ym)
+        insert_month_chart(ym.year, ym.month)
+
+    drop_table_if_exists("month_chart")
+    create_month_chart_table("month_chart")
+    with MariaConnection() as conn:
+        conn.query(f"insert into chart (select * from {table_name})")
+        conn.commit()
+
+
+if __name__ == '__main__':
+    upload_chart_from_krx()
+    upload_month_chart()
