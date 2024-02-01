d809e9e0f5d0d36af94f38b42cf4c026d1691361
wookjaejo <wookjae.jo@ybrain.com>
2024-02-01 09:28:48 +0900

    주기적 데이터 수집 코드 수정

--- manage.py
+++ manage.py
@@ -1,9 +1,27 @@
+"""
+매월 말일 또는 다음달 초 아래 데이터 수집
+0. Index chart
+update_index_chart(date(2024, 1, 1)) 통해 지수 데이터 수집
+
+0. Daily stock prices
+update_chart(date(2024, 1, 1)) 통해 일별 주가 데이터 수집
+
+0. Monthly stock prices
+insert_month_chart(2024, 1)
+
+0. Financial data
+
+
+"""
+
 from datetime import date
-from core.repository.maria.manage import update_chart
+from core.repository.maria.manage import update_chart, update_index_chart, insert_month_chart
 
 
 def main():
-    update_chart(date(1997, 1, 1))
+    update_index_chart(date(2024, 1, 1))
+    update_chart(date(2024, 1, 1))
+    insert_month_chart(2024, 1)
 
 
 if __name__ == '__main__':
